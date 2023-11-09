c2090007cb5a12e5c2af4145c34bec57f75d4b2e
Mara <125482894+Mara2403@users.noreply.github.com>
2023-11-09 12:19:40 +0100

    Add substring method, counting substrings

--- sub_strings.rb
+++ sub_strings.rb
@@ -1,3 +1,25 @@
-def substrings(word, dictonary)
+dictonary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
+
+def substrings(user_words, dictonary)
+
+user_array = user_words.downcase.split(/\W+/)
+
+found_words_hash = {}
+
+user_array.each do |user_array|
+  dictonary.each do |word|
+    if user_array.include?(word)
+      if found_words_hash.key?(word)
+        found_words_hash[word] += 1
+      else
+        found_words_hash[word] = 1
+      end
+    end
+  end
+end
+
+puts found_words_hash
 
 end
+
+substrings("Howdy partner, sit down! How's it going?", dictonary)
